5637b249ae973020c0930109ee02365443a7eade
MairianeTSoul <marysoul2308@gmail.com>
2024-12-21 10:43:49 +0300

    lab-3 : Quick Sort

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <random>
 #include <vector>
 #include <fstream>
+#include <filesystem>
 
 #include <gtest/gtest.h>
 
@@ -12,7 +13,11 @@ using namespace chrono;
 
 void save_results_to_csv(const vector<int>& sizes, const vector<double>& rev_is, const vector<double>& rev_qs, const vector<double>& rev_cqs)
 {
-    ofstream file("Data_" + std::to_string(treshold) + "_" + std::to_string(rev_qs[rev_cqs.size() - 1] * 1000000) + "_.csv");
+    filesystem::create_directory("result");
+    string file_path = "result/data_" + std::to_string(treshold) + "_" +
+                       std::to_string(static_cast<int>(rev_qs[rev_cqs.size() - 1] * 1000000)) + "_.csv";
+
+    ofstream file(file_path);
 
     if (!file.is_open())
     {
@@ -29,123 +34,111 @@ void save_results_to_csv(const vector<int>& sizes, const vector<double>& rev_is,
     file.close();
 }
 
-//
-//TEST(InsertionSortTest, SortSmallArray) {
-//    int arr[] = {5, 3, 4, 1, 2};
-//    insertion_sort(arr, arr + 5, [](int a, int b) { return a < b; });
-//    EXPECT_EQ(arr[0], 1);
-//    EXPECT_EQ(arr[1], 2);
-//    EXPECT_EQ(arr[2], 3);
-//    EXPECT_EQ(arr[3], 4);
-//    EXPECT_EQ(arr[4], 5);
-//}
-//
-//TEST(InsertionSortTest, EmptyArray) {
-//    int arr[] = {};
-//    insertion_sort(arr, arr + 0, [](int a, int b) { return a < b; });
-//    EXPECT_EQ(arr, arr + 0);
-//}
-//
-//
-//TEST(InsertionSortTest, SingleElementArray) {
-//    int arr[] = {10};
-//    insertion_sort(arr, arr + 1, [](int a, int b) { return a < b; });
-//    EXPECT_EQ(arr[0], 10);
-//}
-//
-//TEST(InsertionSortTest, ArrayWithIdenticalElements) {
-//    int arr[] = {7, 7, 7, 7};
-//    insertion_sort(arr, arr + 4, [](int a, int b) { return a < b; });
-//    EXPECT_EQ(arr[0], 7);
-//    EXPECT_EQ(arr[1], 7);
-//    EXPECT_EQ(arr[2], 7);
-//    EXPECT_EQ(arr[3], 7);
-//}
-//
-//TEST(MedianOfThreeTest, CorrectMedian) {
-//    int arr[] = {4, 5, 7, 10, 1, 2, -1};
-//
-//    int *first = arr;
-//    int *last = arr + 7;
-//    int *mid = first + (last - first) / 2;
-//
-//    auto median = median_of_three(first, mid, last, [](int a, int b) { return a < b; });
-//    EXPECT_EQ(median, 4);
-//}
-//
-//
-//TEST(QuickSortTest, SortArray) {
-//    int arr[] = {10, 5, 3, 7, 6, 1, 9, 8, 4, 2};
-//    combo_sort(arr, arr + 10, [](int a, int b) { return a < b; });
-//    for (int i = 0; i < 9; ++i) {
-//        EXPECT_LE(arr[i], arr[i + 1]);
-//    }
-//}
-//
-//TEST(QuickSortTest, SortWithDoubleAndNegativeNumbers) {
-//    double arr[] = {1.4, -2.1, 3.4, -4.3, 5.33, 0.1};
-//    combo_sort(arr, arr + 6, [](double a, double b) { return a < b; });
-//    EXPECT_EQ(arr[0], -4.3);
-//    EXPECT_EQ(arr[1], -2.1);
-//    EXPECT_EQ(arr[2], 0.1);
-//    EXPECT_EQ(arr[3], 1.4);
-//    EXPECT_EQ(arr[4], 3.4);
-//    EXPECT_EQ(arr[5], 5.33);
-//}
-//
-//TEST(QuickSortTest, SortLargeArray) {
-//    const int size = 100000;
-//    int arr[size];
-//    for (int i = 0; i < size; ++i) {
-//        arr[i] = size - i;
-//    }
-//    combo_sort(arr, arr + size, [](int a, int b) { return a < b; });
-//    for (int i = 0; i < size - 1; ++i) {
-//        EXPECT_LE(arr[i], arr[i + 1]);
-//    }
-//}
-//
-//TEST(SortingTest, SortStringsAlphabetically) {
-//    string strings[] = {"banana", "apple", "cherry", "date"};
-//    string expected[] = {"apple", "banana", "cherry", "date"};
-//    int size = sizeof(strings) / sizeof(strings[0]);
-//
-//    combo_sort(strings, strings + size, [](const string &a, const string &b) {
-//        return a < b;
-//    });
-//
-//    for (int i = 0; i < size; ++i) {
-//        EXPECT_EQ(strings[i], expected[i]);
-//    }
-//}
-//
-//TEST(SortingTest, SortStringsByLength) {
-//    string strings[] = {"kiwi", "pineapple", "pear", "apple"};
-//    string expected[] = {"kiwi", "pear", "apple", "pineapple"};
-//    int size = sizeof(strings) / sizeof(strings[0]);
-//
-//    combo_sort(strings, strings + size, [](const string &a, const string &b) {
-//        return a.size() < b.size();
-//    });
-//
-//    for (int i = 0; i < size; ++i) {
-//        EXPECT_EQ(strings[i], expected[i]);
-//    }
-//}
-//
-//TEST(SortingTest, SortStringsReverseAlphabetical) {
-//    string strings[] = {"banana", "apple", "cherry", "date"};
-//    string expected[] = {"date", "cherry", "banana", "apple"};
-//    int size = sizeof(strings) / sizeof(strings[0]);
-//
-//    combo_sort(strings, strings + size, [](const string &a, const string &b) {
-//        return a > b;
-//    });
-//
-//    for (int i = 0; i < size; ++i) {
-//        EXPECT_EQ(strings[i], expected[i]);
-//    }
-//}
+
+TEST(InsertionSortTest, SortSmallArray) {
+    int arr[] = {5, 3, 4, 1, 2};
+    insertion_sort(arr, arr + 5, [](int a, int b) { return a < b; });
+    EXPECT_EQ(arr[0], 1);
+    EXPECT_EQ(arr[1], 2);
+    EXPECT_EQ(arr[2], 3);
+    EXPECT_EQ(arr[3], 4);
+    EXPECT_EQ(arr[4], 5);
+}
+
+TEST(InsertionSortTest, EmptyArray) {
+    int arr[] = {};
+    insertion_sort(arr, arr + 0, [](int a, int b) { return a < b; });
+    EXPECT_EQ(arr, arr + 0);
+}
+
+
+TEST(InsertionSortTest, SingleElementArray) {
+    int arr[] = {10};
+    insertion_sort(arr, arr + 1, [](int a, int b) { return a < b; });
+    EXPECT_EQ(arr[0], 10);
+}
+
+TEST(InsertionSortTest, ArrayWithIdenticalElements) {
+    int arr[] = {7, 7, 7, 7};
+    insertion_sort(arr, arr + 4, [](int a, int b) { return a < b; });
+    EXPECT_EQ(arr[0], 7);
+    EXPECT_EQ(arr[1], 7);
+    EXPECT_EQ(arr[2], 7);
+    EXPECT_EQ(arr[3], 7);
+}
+
+TEST(QuickSortTest, SortArray) {
+    int arr[] = {10, 5, 3, 7, 6, 1, 9, 8, 4, 2};
+    combo_sort(arr, arr + 10, [](int a, int b) { return a < b; });
+    for (int i = 0; i < 9; ++i) {
+        EXPECT_LE(arr[i], arr[i + 1]);
+    }
+}
+
+TEST(QuickSortTest, SortWithDoubleAndNegativeNumbers) {
+    double arr[] = {1.4, -2.1, 3.4, -4.3, 5.33, 0.1};
+    combo_sort(arr, arr + 6, [](double a, double b) { return a < b; });
+    EXPECT_EQ(arr[0], -4.3);
+    EXPECT_EQ(arr[1], -2.1);
+    EXPECT_EQ(arr[2], 0.1);
+    EXPECT_EQ(arr[3], 1.4);
+    EXPECT_EQ(arr[4], 3.4);
+    EXPECT_EQ(arr[5], 5.33);
+}
+
+TEST(QuickSortTest, SortLargeArray) {
+    const int size = 100000;
+    int arr[size];
+    for (int i = 0; i < size; ++i) {
+        arr[i] = size - i;
+    }
+    combo_sort(arr, arr + size, [](int a, int b) { return a < b; });
+    for (int i = 0; i < size - 1; ++i) {
+        EXPECT_LE(arr[i], arr[i + 1]);
+    }
+}
+
+TEST(SortingTest, SortStringsAlphabetically) {
+    string strings[] = {"banana", "apple", "cherry", "date"};
+    string expected[] = {"apple", "banana", "cherry", "date"};
+    int size = sizeof(strings) / sizeof(strings[0]);
+
+    combo_sort(strings, strings + size, [](const string &a, const string &b) {
+        return a < b;
+    });
+
+    for (int i = 0; i < size; ++i) {
+        EXPECT_EQ(strings[i], expected[i]);
+    }
+}
+
+TEST(SortingTest, SortStringsByLength) {
+    string strings[] = {"kiwi", "pineapple", "pear", "apple"};
+    string expected[] = {"kiwi", "pear", "apple", "pineapple"};
+    int size = sizeof(strings) / sizeof(strings[0]);
+
+    combo_sort(strings, strings + size, [](const string &a, const string &b) {
+        return a.size() < b.size();
+    });
+
+    for (int i = 0; i < size; ++i) {
+        EXPECT_EQ(strings[i], expected[i]);
+    }
+}
+
+TEST(SortingTest, SortStringsReverseAlphabetical) {
+    string strings[] = {"banana", "apple", "cherry", "date"};
+    string expected[] = {"date", "cherry", "banana", "apple"};
+    int size = sizeof(strings) / sizeof(strings[0]);
+
+    combo_sort(strings, strings + size, [](const string &a, const string &b) {
+        return a > b;
+    });
+
+    for (int i = 0; i < size; ++i) {
+        EXPECT_EQ(strings[i], expected[i]);
+    }
+}
 
 void insertion_sort_test(vector<int> arr)
 {
@@ -174,11 +167,12 @@ vector<int> generateArray(int size) {
 }
 
 int main(int argc, char** argv) {
-    for (treshold = 28; treshold <= 36; treshold++)
+    for (treshold = 32; treshold <= 32; treshold++)
     {
         cout << treshold << endl;
 
-        //    ::testing::InitGoogleTest(&argc, argv);
+        ::testing::InitGoogleTest(&argc, argv);
+
         vector<double> insertion_sort_times;
         vector<double> quick_sort_times;
         vector<double> combo_sort_times;
@@ -233,9 +227,9 @@ int main(int argc, char** argv) {
 
         save_results_to_csv(testSizes, insertion_sort_times, quick_sort_times, combo_sort_times);
 
-        //cout << "Test 3" << endl;
 
-        //    return RUN_ALL_TESTS();
     }
-    return 0;
+
+    cout << "Test 3" << endl;
+    return RUN_ALL_TESTS();
 }
