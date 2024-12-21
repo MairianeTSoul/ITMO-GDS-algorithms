a956050d46beeeec91a3fede47fa2bd874cba300
MairianeTSoul <marysoul2308@gmail.com>
2024-12-21 10:32:34 +0300

    lab-3 : Quick Sort

--- main.cpp
+++ main.cpp
@@ -3,233 +3,239 @@
 #include "sorting.hpp"
 #include <random>
 #include <vector>
+#include <fstream>
+
 #include <gtest/gtest.h>
 
-void generateArray(int* arr, int size) {
-    for (int i = 0; i < size; ++i) {
-        arr[i] = size - i;
+using namespace std;
+using namespace chrono;
+
+void save_results_to_csv(const vector<int>& sizes, const vector<double>& rev_is, const vector<double>& rev_qs, const vector<double>& rev_cqs)
+{
+    ofstream file("Data_" + std::to_string(treshold) + "_" + std::to_string(rev_qs[rev_cqs.size() - 1] * 1000000) + "_.csv");
+
+    if (!file.is_open())
+    {
+        cerr << "Cann't open Data.csv\n";
+        return;
     }
-}
 
-TEST(InsertionSortTest, SortSmallArray) {
-    int arr[] = {5, 3, 4, 1, 2};
-    insertion_sort(arr, arr + 5, [](int a, int b) { return a < b; });
-    EXPECT_EQ(arr[0], 1);
-    EXPECT_EQ(arr[1], 2);
-    EXPECT_EQ(arr[2], 3);
-    EXPECT_EQ(arr[3], 4);
-    EXPECT_EQ(arr[4], 5);
-}
+    file << "size,insertion_sort,quick_sort,combo_sort\n";
 
-TEST(InsertionSortTest, EmptyArray) {
-    int arr[] = {};
-    insertion_sort(arr, arr + 0, [](int a, int b) { return a < b; });
-    EXPECT_EQ(arr, arr + 0);
-}
-
-
-TEST(InsertionSortTest, SingleElementArray) {
-    int arr[] = {10};
-    insertion_sort(arr, arr + 1, [](int a, int b) { return a < b; });
-    EXPECT_EQ(arr[0], 10);
-}
-
-TEST(InsertionSortTest, ArrayWithIdenticalElements) {
-    int arr[] = {7, 7, 7, 7};
-    insertion_sort(arr, arr + 4, [](int a, int b) { return a < b; });
-    EXPECT_EQ(arr[0], 7);
-    EXPECT_EQ(arr[1], 7);
-    EXPECT_EQ(arr[2], 7);
-    EXPECT_EQ(arr[3], 7);
-}
-
-TEST(MedianOfThreeTest, CorrectMedian) {
-    int arr[] = {4, 5, 7, 10, 1, 2, -1};
-
-    int *first = arr;
-    int *last = arr + 7;
-    int *mid = first + (last - first) / 2;
-
-    auto median = median_of_three(first, mid, last, [](int a, int b) { return a < b; });
-    EXPECT_EQ(median, 4);
-}
-
-
-TEST(QuickSortTest, SortArray) {
-    int arr[] = {10, 5, 3, 7, 6, 1, 9, 8, 4, 2};
-    combo_sort(arr, arr + 10, [](int a, int b) { return a < b; });
-    for (int i = 0; i < 9; ++i) {
-        EXPECT_LE(arr[i], arr[i + 1]);
+    for (size_t i = 0; i < sizes.size(); ++i) {
+        file << sizes[i] << "," << rev_is[i] * 1e9 << "," << rev_qs[i] * 1e9 << "," << rev_cqs[i] * 1e9 << "\n";
     }
+
+    file.close();
 }
 
-TEST(QuickSortTest, SortWithDoubleAndNegativeNumbers) {
-    double arr[] = {1.4, -2.1, 3.4, -4.3, 5.33, 0.1};
-    combo_sort(arr, arr + 6, [](double a, double b) { return a < b; });
-    EXPECT_EQ(arr[0], -4.3);
-    EXPECT_EQ(arr[1], -2.1);
-    EXPECT_EQ(arr[2], 0.1);
-    EXPECT_EQ(arr[3], 1.4);
-    EXPECT_EQ(arr[4], 3.4);
-    EXPECT_EQ(arr[5], 5.33);
+//
+//TEST(InsertionSortTest, SortSmallArray) {
+//    int arr[] = {5, 3, 4, 1, 2};
+//    insertion_sort(arr, arr + 5, [](int a, int b) { return a < b; });
+//    EXPECT_EQ(arr[0], 1);
+//    EXPECT_EQ(arr[1], 2);
+//    EXPECT_EQ(arr[2], 3);
+//    EXPECT_EQ(arr[3], 4);
+//    EXPECT_EQ(arr[4], 5);
+//}
+//
+//TEST(InsertionSortTest, EmptyArray) {
+//    int arr[] = {};
+//    insertion_sort(arr, arr + 0, [](int a, int b) { return a < b; });
+//    EXPECT_EQ(arr, arr + 0);
+//}
+//
+//
+//TEST(InsertionSortTest, SingleElementArray) {
+//    int arr[] = {10};
+//    insertion_sort(arr, arr + 1, [](int a, int b) { return a < b; });
+//    EXPECT_EQ(arr[0], 10);
+//}
+//
+//TEST(InsertionSortTest, ArrayWithIdenticalElements) {
+//    int arr[] = {7, 7, 7, 7};
+//    insertion_sort(arr, arr + 4, [](int a, int b) { return a < b; });
+//    EXPECT_EQ(arr[0], 7);
+//    EXPECT_EQ(arr[1], 7);
+//    EXPECT_EQ(arr[2], 7);
+//    EXPECT_EQ(arr[3], 7);
+//}
+//
+//TEST(MedianOfThreeTest, CorrectMedian) {
+//    int arr[] = {4, 5, 7, 10, 1, 2, -1};
+//
+//    int *first = arr;
+//    int *last = arr + 7;
+//    int *mid = first + (last - first) / 2;
+//
+//    auto median = median_of_three(first, mid, last, [](int a, int b) { return a < b; });
+//    EXPECT_EQ(median, 4);
+//}
+//
+//
+//TEST(QuickSortTest, SortArray) {
+//    int arr[] = {10, 5, 3, 7, 6, 1, 9, 8, 4, 2};
+//    combo_sort(arr, arr + 10, [](int a, int b) { return a < b; });
+//    for (int i = 0; i < 9; ++i) {
+//        EXPECT_LE(arr[i], arr[i + 1]);
+//    }
+//}
+//
+//TEST(QuickSortTest, SortWithDoubleAndNegativeNumbers) {
+//    double arr[] = {1.4, -2.1, 3.4, -4.3, 5.33, 0.1};
+//    combo_sort(arr, arr + 6, [](double a, double b) { return a < b; });
+//    EXPECT_EQ(arr[0], -4.3);
+//    EXPECT_EQ(arr[1], -2.1);
+//    EXPECT_EQ(arr[2], 0.1);
+//    EXPECT_EQ(arr[3], 1.4);
+//    EXPECT_EQ(arr[4], 3.4);
+//    EXPECT_EQ(arr[5], 5.33);
+//}
+//
+//TEST(QuickSortTest, SortLargeArray) {
+//    const int size = 100000;
+//    int arr[size];
+//    for (int i = 0; i < size; ++i) {
+//        arr[i] = size - i;
+//    }
+//    combo_sort(arr, arr + size, [](int a, int b) { return a < b; });
+//    for (int i = 0; i < size - 1; ++i) {
+//        EXPECT_LE(arr[i], arr[i + 1]);
+//    }
+//}
+//
+//TEST(SortingTest, SortStringsAlphabetically) {
+//    string strings[] = {"banana", "apple", "cherry", "date"};
+//    string expected[] = {"apple", "banana", "cherry", "date"};
+//    int size = sizeof(strings) / sizeof(strings[0]);
+//
+//    combo_sort(strings, strings + size, [](const string &a, const string &b) {
+//        return a < b;
+//    });
+//
+//    for (int i = 0; i < size; ++i) {
+//        EXPECT_EQ(strings[i], expected[i]);
+//    }
+//}
+//
+//TEST(SortingTest, SortStringsByLength) {
+//    string strings[] = {"kiwi", "pineapple", "pear", "apple"};
+//    string expected[] = {"kiwi", "pear", "apple", "pineapple"};
+//    int size = sizeof(strings) / sizeof(strings[0]);
+//
+//    combo_sort(strings, strings + size, [](const string &a, const string &b) {
+//        return a.size() < b.size();
+//    });
+//
+//    for (int i = 0; i < size; ++i) {
+//        EXPECT_EQ(strings[i], expected[i]);
+//    }
+//}
+//
+//TEST(SortingTest, SortStringsReverseAlphabetical) {
+//    string strings[] = {"banana", "apple", "cherry", "date"};
+//    string expected[] = {"date", "cherry", "banana", "apple"};
+//    int size = sizeof(strings) / sizeof(strings[0]);
+//
+//    combo_sort(strings, strings + size, [](const string &a, const string &b) {
+//        return a > b;
+//    });
+//
+//    for (int i = 0; i < size; ++i) {
+//        EXPECT_EQ(strings[i], expected[i]);
+//    }
+//}
+
+void insertion_sort_test(vector<int> arr)
+{
+    vector<int> arr_copy = arr;
+    insertion_sort(arr_copy.data(), arr_copy.data() + arr_copy.size(), [](int a, int b) { return a < b; });
 }
 
-TEST(QuickSortTest, SortLargeArray) {
-    const int size = 100000;
-    int arr[size];
-    for (int i = 0; i < size; ++i) {
-        arr[i] = size - i;
-    }
-    combo_sort(arr, arr + size, [](int a, int b) { return a < b; });
-    for (int i = 0; i < size - 1; ++i) {
-        EXPECT_LE(arr[i], arr[i + 1]);
-    }
+void quick_sort_test(vector<int> arr)
+{
+    vector<int> arr_copy = arr;
+    quick_sort(arr.data(), arr.data() + arr.size(), [](int a, int b) { return a < b; });
 }
 
-TEST(SortingTest, SortStringsAlphabetically) {
-    std::string strings[] = {"banana", "apple", "cherry", "date"};
-    std::string expected[] = {"apple", "banana", "cherry", "date"};
-    int size = sizeof(strings) / sizeof(strings[0]);
 
-    combo_sort(strings, strings + size, [](const std::string &a, const std::string &b) {
-        return a < b;
-    });
-
-    for (int i = 0; i < size; ++i) {
-        EXPECT_EQ(strings[i], expected[i]);
-    }
+void combo_sort_test(vector<int> arr)
+{
+    vector<int> arr_copy = arr;
+    combo_sort(arr.data(), arr.data() + arr.size(), [](int a, int b) { return a < b; });
 }
 
-TEST(SortingTest, SortStringsByLength) {
-    std::string strings[] = {"kiwi", "pineapple", "pear", "apple"};
-    std::string expected[] = {"kiwi", "pear", "apple", "pineapple"};
-    int size = sizeof(strings) / sizeof(strings[0]);
 
-    combo_sort(strings, strings + size, [](const std::string &a, const std::string &b) {
-        return a.size() < b.size();
-    });
-
-    for (int i = 0; i < size; ++i) {
-        EXPECT_EQ(strings[i], expected[i]);
-    }
-}
-
-TEST(SortingTest, SortStringsReverseAlphabetical) {
-    std::string strings[] = {"banana", "apple", "cherry", "date"};
-    std::string expected[] = {"date", "cherry", "banana", "apple"};
-    int size = sizeof(strings) / sizeof(strings[0]);
-
-    combo_sort(strings, strings + size, [](const std::string &a, const std::string &b) {
-        return a > b;
-    });
-
-    for (int i = 0; i < size; ++i) {
-        EXPECT_EQ(strings[i], expected[i]);
-    }
-}
-
-void runBenchmark(const std::string& sortName, std::function<void(int*, int*)> sortFunc) {
-    ankerl::nanobench::Bench bench;
-    std::cout << sortName << std::endl;
-    bench.title(sortName).relative(true);
-
-    std::vector<int> sizes;
-
-    for (int i = 10000; i < 100000; i += 10000) sizes.push_back(i);
-    for (int i = 100000; i <= 1000000; i += 100000) sizes.push_back(i);
-
-    for (int size : sizes) {
-        std::vector<int> arr(size);
-
-        generateArray(arr.data(), size);
-
-        bench.run(std::to_string(size) + " elements", [&] {
-            sortFunc(arr.data(), arr.data() + size);
-            ankerl::nanobench::doNotOptimizeAway(arr);
-        });
-    }
+vector<int> generateArray(int size) {
+    vector<int> arr(size);
+    generate(arr.begin(), arr.end(), [n = size]() mutable { return --n; });
+    return arr;
 }
 
 int main(int argc, char** argv) {
+    for (treshold = 28; treshold <= 36; treshold++)
+    {
+        cout << treshold << endl;
 
-    ::testing::InitGoogleTest(&argc, argv);
+        //    ::testing::InitGoogleTest(&argc, argv);
+        vector<double> insertion_sort_times;
+        vector<double> quick_sort_times;
+        vector<double> combo_sort_times;
 
-    std::cout << "Test 1" << std::endl;
+        vector<int> arr;
 
-    runBenchmark("sort", [](int* begin, int* end) {
-        combo_sort(begin, end, [](int a, int b) { return a < b; });
-    });
+        int warmups = 1000, epochs = 10000;
+        ankerl::nanobench::Bench bench;
 
-    runBenchmark("quick sort", [](int* begin, int* end) {
-        quick_sort(begin, end, [](int a, int b) { return a < b; });
-    });
-
-    runBenchmark("insertion sort", [](int* begin, int* end) {
-        insertion_sort(begin, end, [](int a, int b) { return a < b; });
-    });
-
-    std::cout << "Test 2" << std::endl;
-
-    int testSizes[20];
-
-    for (int i = 0; i < 20; i++) {
-        testSizes[i] = (i + 2) * 2;
-    }
-
-//    std::vector<int> arr_test(10);
-//    for (int i = 0; i < 10; ++i) {
-//        generateArray(arr_test.data(), 10);
-//        quick_sort(arr_test.data(), arr_test.data() + 3, [](int a, int b) { return a < b; });
-//    }
-//    for (int i = 0; i < 10; ++i) {
-//        generateArray(arr_test.data(), 10);
-//        insertion_sort(arr_test.data(), arr_test.data() + 3, [](int a, int b) { return a < b; });
-//    }
-//
-//    for (int i = 0; i < 10; ++i) {
-//        generateArray(arr_test.data(), 10);
-//        combo_sort(arr_test.data(), arr_test.data() + 3, [](int a, int b) { return a < b; });
-//    }
-
-    for (int size: testSizes) {
-        std::vector<int> arr(size);
-        generateArray(arr.data(), size);
-
-        auto start = std::chrono::high_resolution_clock::now();
-        insertion_sort(arr.data(), arr.data() + size, [](int a, int b) { return a < b; });
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> insertionSortTime = end - start;
-
-        generateArray(arr.data(), size);
-
-        start = std::chrono::high_resolution_clock::now();
-        quick_sort(arr.data(), arr.data() + size, [](int a, int b) { return a < b; });
-        end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> quickSortTime = end - start;
-
-        generateArray(arr.data(), size);
-
-        start = std::chrono::high_resolution_clock::now();
-        combo_sort(arr.data(), arr.data() + size, [](int a, int b) { return a < b; });
-        end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> sortTime = end - start;
-
-        std::cout << "Size: " << size
-                  << "  insertion sort: " << insertionSortTime.count() * 1e9
-                  << "ns, quick sort: " << quickSortTime.count() * 1e9
-                  << "ns, sort: " << sortTime.count() * 1e9 << "ns min: ";
-
-        if (insertionSortTime < quickSortTime && insertionSortTime < sortTime) {
-            std::cout << "insertion sort\n";
-        } else if (quickSortTime < insertionSortTime && quickSortTime < sortTime) {
-            std::cout << "quick sort\n";
-        } else {
-            std::cout << "sort\n";
+        vector<int>testSizes;
+        for (int i = 1; i <= 60; i++) {
+            testSizes.push_back(i);
         }
+
+        for (int size : testSizes) {
+            cout << "Size: " << size << endl;
+            arr = generateArray(size);
+
+            bench = ankerl::nanobench::Bench().warmup(warmups).minEpochIterations(epochs);
+            bench.run("Insertion sort", [&]() {insertion_sort_test(arr); });
+            insertion_sort_times.push_back(bench.results().front().median(ankerl::nanobench::Result::Measure::elapsed));
+
+            bench = ankerl::nanobench::Bench().warmup(warmups).minEpochIterations(epochs);
+            bench.run("Default Quick sort", [&]() {quick_sort_test(arr); });
+            quick_sort_times.push_back(bench.results().front().median(ankerl::nanobench::Result::Measure::elapsed));
+
+
+            bench = ankerl::nanobench::Bench().warmup(warmups).minEpochIterations(epochs);
+            bench.run("Quick sort", [&]() {combo_sort_test(arr); });
+            combo_sort_times.push_back(bench.results().front().median(ankerl::nanobench::Result::Measure::elapsed));
+
+            cout << "Size: " << size;
+
+            auto start = high_resolution_clock::now();
+            insertion_sort_test(arr);
+            auto end = high_resolution_clock::now();
+            duration<double> insertionSortTime = end - start;
+            cout << "; Insertion Sort Time: " << duration_cast<nanoseconds>(insertionSortTime).count() << " ns";
+
+            start = high_resolution_clock::now();
+            quick_sort_test(arr);
+            end = high_resolution_clock::now();
+            duration<double> quickSortTime = end - start;
+            cout << "; Quick Sort Time: " << duration_cast<nanoseconds>(quickSortTime).count() << " ns";
+
+            start = high_resolution_clock::now();
+            combo_sort_test(arr);
+            end = high_resolution_clock::now();
+            duration<double> sortTime = end - start;
+            cout << "; Combo Sort Time: " << duration_cast<nanoseconds>(sortTime).count() << " ns" << endl;
+        }
+
+        save_results_to_csv(testSizes, insertion_sort_times, quick_sort_times, combo_sort_times);
+
+        //cout << "Test 3" << endl;
+
+        //    return RUN_ALL_TESTS();
     }
-
-
-    std::cout << "Test 3" << std::endl;
-
-    return RUN_ALL_TESTS();
+    return 0;
 }
--- sorting.hpp
+++ sorting.hpp
@@ -3,76 +3,167 @@
 #include <functional>
 #include <utility>
 
+int treshold = 32;
+
+using namespace std;
+
 template<typename T, typename Compare>
 void insertion_sort(T* first, T* last, Compare comp) {
-    for (T* i = first + 1; i != last; ++i) {
-        T* j = i;
-        while (j > first && comp(*j, *(j - 1))) {
-            std::swap(*j, *(j - 1));
-            --j;
+    for (T* i = first + 1; i < last; ++i)
+    {
+        T value = move(*i);
+        T* temp = i;
+        while (temp > first && comp(value, *(temp - 1)))
+        {
+            *temp = move(*(temp - 1));
+            --temp;
         }
+        *temp = move(value);
     }
 }
 
 template <typename T, typename Compare>
 T& median_of_three(T* first, T* mid, T* last, Compare comp) {
-    if (comp(*mid, *first)) std::swap(*mid, *first);
-    if (comp(*(last - 1), *first)) std::swap(*(last - 1), *first);
-    if (comp(*(last - 1), *mid)) std::swap(*(last - 1), *mid);
+    if (comp(*mid, *first))
+    {
+        T temp = move(*mid);
+        *mid = move(*first);
+        *first = move(temp);
+    }
+
+    if (comp(*last, *first))
+    {
+        T temp = move(*last);
+        *last = move(*first);
+        *first = move(temp);
+    }
+
+    if (comp(*last, *mid))
+    {
+        T temp = move(*last);
+        *last = move(*mid);
+        *mid = move(temp);
+    }
+
     return *mid;
 }
 
 template<typename T, typename Compare>
-T* partition(T* first, T* last, Compare comp) {
-    T* mid = first + (last - first) / 2;
-    median_of_three(first, mid, last, comp);
-    std::swap(*(last - 1), *mid);
+double violation_coeff(T* first, T* last, Compare comp)
+{
+    int violations = 0;
 
-    T* i = first;
-    for (T* j = first; j < last - 1; ++j) {
-        if (comp(*j, *(last - 1))) {
-            std::swap(*i, *j);
-            ++i;
+    for (T* it = first + 1; it < last; ++it) {
+        if (comp(*(it - 1), *it)) {
+            ++violations;
         }
     }
 
-    std::swap(*(last - 1), *i);
-    return i;
+    return static_cast<double>(violations) / (distance(first, last) - 1);
 }
+
 
 template<typename T, typename Compare>
 void quick_sort(T* first, T* last, Compare comp) {
-    while (first < last) {
-        T* pivot = partition(first, last, comp);
+    while (distance(first, last) > 1)
+    {
+        int size = distance(first, last);
 
-        if (pivot - first < last - pivot) {
-            combo_sort(first, pivot, comp);
-            first = pivot + 1;
-        } else {
-            combo_sort(pivot + 1, last, comp);
-            last = pivot - 1;
+        T reference_element = median_of_three(first, first + (size / 2), last - 1, comp);
+
+        T* left = first;
+        T* right = last - 1;
+
+        while (true)
+        {
+            while (comp(*left, reference_element)) {
+                ++left;
+            }
+
+            while (comp(reference_element, *right)) {
+                --right;
+            }
+
+            if (left >= right) {
+                break;
+            }
+
+            T temp = move(*left);
+            *left = move(*right);
+            *right = move(temp);
+
+            ++left;
+            --right;
+        }
+
+        T* mid = left;
+
+        if (distance(first, mid) < distance(mid, last))
+        {
+            quick_sort(first, mid, comp);
+            first = mid;
+        }
+        else
+        {
+            quick_sort(mid, last, comp);
+            last = mid;
         }
     }
 }
 
 template<typename T, typename Compare>
 void combo_sort(T* first, T* last, Compare comp) {
-    const int threshold = 12;
+    while (distance(first, last) > 1)
+    {
+        int size = distance(first, last);
 
-    while (first < last) {
-        if (last - first <= threshold) {
-            insertion_sort(first, last, comp);
-            break;
+        if (size <= treshold)
+        {
+            if (violation_coeff(first, last, comp) > 0.1)
+            {
+                insertion_sort(first, last, comp);
+                return;
+            }
         }
-        T* pivot = partition(first, last, comp);
 
-        if (pivot - first < last - pivot) {
-            combo_sort(first, pivot, comp);
-            first = pivot + 1;
-        } else {
-            combo_sort(pivot + 1, last, comp);
-            last = pivot - 1;
+        T reference_element = median_of_three(first, first + (size / 2), last - 1, comp);
+
+        T* left = first;
+        T* right = last - 1;
+
+        while (true)
+        {
+            while (comp(*left, reference_element)) {
+                ++left;
+            }
+
+            while (comp(reference_element, *right)) {
+                --right;
+            }
+
+            if (left >= right) {
+                break;
+            }
+
+            T temp = move(*left);
+            *left = move(*right);
+            *right = move(temp);
+
+            ++left;
+            --right;
+        }
+
+        T* mid = left;
+
+        if (distance(first, mid) < distance(mid, last))
+        {
+            combo_sort(first, mid, comp);
+            first = mid;
+        }
+        else
+        {
+            combo_sort(mid, last, comp);
+            last = mid;
         }
     }
 }
-
